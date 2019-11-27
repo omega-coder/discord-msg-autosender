51d653d963b784a2a2bfd9ce887b010feaadda5d
CHERIEF Yassine <fy_cherief@esi.dz>
2019-11-27 22:33:38 +0100

    Replaces discord_channel_url with a server_name

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ dotenv.config();
 module.exports = {
     discord_password: process.env.DISCORD_PASSWORD,
     discord_user: process.env.DISCORD_USER,
-    discord_channel_url: process.env.DISCORD_CHANNEL_URL
+    server_name: process.env.SERVER_NAME
 };
