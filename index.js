9991fec5bec052380a6bfdfa6712de53db9bbc4e
CHERIEF Yassine <fy_cherief@esi.dz>
2019-11-28 02:00:31 +0100

    Adds proper logging abd fixes server_name unhandled in Promise

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const axios = require('axios');
 const puppeteer = require('puppeteer');
 const fs = require("fs");
 const inquirer = require("inquirer");
+const chalk = require("chalk");
+const error = chalk.bold.red;
+const warning = chalk.bold.keyword('orange');
+
+const log = console.log;
 
 const {
     discord_password,
@@ -16,24 +21,32 @@ const DISCORD_APP_URL = "https://discordapp.com/app";
 (async () => {
     const response = await axios.get(`http://localhost:${port}/json/version`);
 
+    if (response.status !== 200) {
+        log(error(`[!] CHECK IF YOUR CHROME/CHROMIUM IS REALLY LISTENING ON PORT ${port}`));
+    } else {
+        log(chalk.bold.green(`[+] Got Response from http://localhost:${port}/json/version`));
+    }
+
     const {
         webSocketDebuggerUrl
     } = response.data;
+    log(chalk.blue(`[?] Got webSocketDebuggerUrl: ${webSocketDebuggerUrl}`));
 
     const browser = await puppeteer.connect({
         browserWSEndpoint: webSocketDebuggerUrl,
         defaultViewport: null
     });
-
-
+    log(chalk.bold.green(`[+] Connected to chrome instance`));
     const page = await browser.newPage();
     await page.setViewport({
-        width: 1200,
-        height: 1000
+        width: 1300,
+        height: 1100
     });
     await page.goto(DISCORD_APP_URL);
 
     if (page.url() === "https://discordapp.com/login") {
+        log(chalk.blue(`[INFO] Got redirect to https://discordapp.com/login`));
+        log(chalk.bold.blue(`[+] Attempting login with given credentials`));
         await page.waitForSelector('[type="email"]');
         await page.evaluate(() => document.querySelector('[type="email"]').value = "");
         await page.focus('[type="email"]');
@@ -48,11 +61,20 @@ const DISCORD_APP_URL = "https://discordapp.com/app";
         });
 
         await page.keyboard.press('Enter');
+        if (page.url() === "https://discordapp.com/activity") {
+            log(chalk.bold.green(`[+] Login Successfull!`));
+        } else {
+            log(error(`[!] Login failed, Exiting now...!`));
+            process.exit(1);
+        }
     }
 
     await page.waitForSelector(`a[aria-label="${server_name}"]`);
-    await page.evaluate(() => document.querySelector(`a[aria-label="${server_name}"]`).click());
-
+    await page.evaluate(({
+        server_name
+    }) => document.querySelector(`a[aria-label="${server_name}"]`).click(), {
+        server_name
+    });
     await autoScroll(page);
 
     await page.waitFor(() => document.querySelectorAll('.containerDefault-1ZnADq').length);
@@ -87,7 +109,7 @@ const DISCORD_APP_URL = "https://discordapp.com/app";
             flood__channel_ = answers.channel[0];
         });
 
-    console.log(flood__channel_);
+    log(chalk.bold.white('[+] Selected channel: ' + chalk.bold.red(`${flood__channel_}`)));
 
     await page.evaluate(({
         flood__channel_
@@ -106,19 +128,23 @@ const DISCORD_APP_URL = "https://discordapp.com/app";
     await page.waitForSelector('textarea');
     await page.focus('textarea');
 
+    log(warning(`[+] Starting channel flood.`));
+
     try {
         // read contents of the file
         const data = fs.readFileSync('mr_robot.txt', 'UTF-8');
         const lines = data.split(/\r?\n/);
-
+        let n_lines = 1;
         for (let l_indx = 0; l_indx < lines.length; l_indx++) {
             await page.keyboard.type(lines[l_indx], {
                 delay: 50
             });
             await page.keyboard.press('Enter');
+            log(`[+] Sent ${n_lines} messages to channel ${flood__channel_}`);
+            n_lines++;
         }
     } catch (err) {
-        console.error(err);
+        log(error(err.toString()));
     }
 
 })();
